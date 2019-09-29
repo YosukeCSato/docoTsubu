245675079b43fdbed629597237419966fd361710
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-09-30 01:19:23 +0900

    applicationスコープに保存していたつぶやきリストを、DBに保存するように変更

--- src/model/Mutter.java
+++ src/model/Mutter.java
@@ -2,16 +2,27 @@ package model;
 
 public class Mutter implements java.io.Serializable {
 
+	private int id; // ID
 	private String userName; // ユーザー名
 	private String text; // つぶやき内容
 
-	public Mutter() {
+	public Mutter() { // 引数なしコンストラクタ
 
 	}
 
 	public Mutter(String userName, String text) {
 		this.userName = userName;
 		this.text = text;
+	}
+
+	public Mutter(int id, String userName, String text) {
+		this.id = id;
+		this.userName = userName;
+		this.text = text;
+	}
+
+	public int getId() {
+		return this.id;
 	}
 
 	public String getUserName() {
